542ccbeffb711000bfc95c09ddf04d648f3cbf86
Sergio De Luca <sdlbsso@gmail.com>
2022-07-06 18:27:19 -0300

    inicia servidor

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection',(socket)=>{
+
+    const idHandShake = socket.id;
+    const { nameRoom } = socket.idHandShake.query;
+
+
+
+})
+
+server.listen(5000,()=>{
+    console.log(`>> Socket listo y escuchando en el puerto: 5000`)
+})
