536561ac6f0fa3147dcd1aaaf0da42fc6554bcd4
unknown <sdlbsso@gmail.com>
2022-07-07 10:20:14 -0300

    servidor node

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const app = express();
+const chalk = require('chalk');
+const cors = require('cors');
+
+app.use(cors());
+const options = {
+  cors: {
+    origin: 'http://localhost:4200',
+  },
+};
+
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-io.on('connection',(socket)=>{
-
-    const idHandShake = socket.id;
-    const { nameRoom } = socket.idHandShake.query;
-    console.log(`Dispositivo: ${idHandShake}`);
+const io = require('socket.io')(server, options);
 
 
 
-})
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-server.listen(5000,()=>{
-    console.log(`>> Socket listo y escuchando en el puerto: 5000`)
+
+
+io.on('connection', function (socket) {
+
+  const handshake = socket.id;
+
+  let { nameRoom } = socket.handshake.query;
+  console.log(`${chalk.green(`Nuevo dispositivo: ${handshake}`)} conentado a la ${nameRoom}`);
+  socket.join(nameRoom)
+
+  socket.on('evento', (res) => {
+    // Emite el mensaje a todos lo miembros de las sala menos a la persona que envia el mensaje   
+    socket.to(nameRoom).emit('evento', res);
+
+  })
+
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(5000, function () {
+  console.log('\n')
+  console.log(`>> Socket listo y escuchando por el puerto: ${chalk.green('5000')}`)
 })
